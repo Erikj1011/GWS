0d2d3321af4ed845ef23c5ae84b55a7279e47838
eriksierra <eriksierra1110@gmail.com>
2025-10-23 14:37:58 -0500

    Subida inicial del laboratorio GWS con estructura visual

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Videojuego, Carrito, ItemCarrito, Compra, DetalleCompra, Resena, Foro
 
-# Register your models here.
+admin.site.register(Videojuego)
+admin.site.register(Carrito)
+admin.site.register(ItemCarrito)
+admin.site.register(Compra)
+admin.site.register(DetalleCompra)
+admin.site.register(Resena)
+admin.site.register(Foro)
